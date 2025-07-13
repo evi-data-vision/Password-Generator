23ab2b7e479475674306270b0e72d899e46d81d8
evi-data-vision <ehsan_7@ymail.com>
2025-07-13 17:58:19 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,46 @@
 import streamlit as st
+import random
+import string
+import nltk
 
-st.write("HIIIIIIIII!!!!!!!!!!!")
+nltk.download('words')
+from nltk.corpus import words
+
+st.title('Password Generator!')
+
+pass_type = st.radio('select your password type :' , ['PIN Code' , 'Random Password' , 'Memorable Password'] , index  = None)
+
+if pass_type == 'PIN Code' :
+    letters = string.digits
+    length = st.slider('Select the length of PIN Code : ' , min_value = 6 , max_value = 32 , value = 8)
+    password = ''.join(random.choices(letters , k = length))
+    st.write('Your Generated PIN Code is : ' , password)
+
+elif pass_type == 'Random Password' :
+     upper = st.toggle('Uppercase Letters')
+     digits = st.toggle('Digits')
+     sign = st.toggle('Special Characters')
+     length = st.slider('Select the length of Password : ' , min_value = 6 , max_value = 32 , value = 8)
+
+     letter = string.ascii_lowercase
+     if upper : letter += string.ascii_uppercase
+     if digits : letter += string.digits
+     if sign : letter += string.punctuation
+
+     password = ''.join(random.choices(letter , k = length))
+     st.write('Your Generated Password is : ' , password)
+
+elif pass_type == 'Memorable Password' :
+    seperator = st.selectbox('Seperator : ' , ['-' , '_' , ',' , '.'])
+    capitalize = st.toggle('Capitalize First Letter of Each Word')
+    
+    if capitalize :
+        word_list = [word.capitalize() for word in words.words()]
+    else : 
+        word_list = [word.lower() for word in words.words()]
+    length = st.slider('Select the number of words : ' , min_value = 2 , max_value = 8 , value = 3)
+    password = seperator.join(random.choices(word_list, k=length))
+    st.write('Your Generated Memorable Password is : ' , password)
+elif pass_type == 'None' :
+    pass
+
